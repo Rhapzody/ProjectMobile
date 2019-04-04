23bc44e0141907373db8592af8d411d7e0b82cfd
thanabodee <thanabodee2661@gmail.com>
2019-04-05 01:49:56 +0700

    แชทครับ

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -25,18 +25,10 @@ export class AboutPage {
 
   }
 
-  async ionViewDidLoad() {
+  ionViewDidLoad() {
     console.log(this.param.data);
     this.user = this.param.data;
-    // let data = await this.chatService.getRoomChat(this.user.email).onSnapshot()
-    
-    // console.log('123');
-    
-
-
     this.chatService.getRoomChat(this.user.email).onSnapshot(data => {
-      console.log('32 about');
-
       let roomtemp = [];
       data.forEach((room) => {
         this.userService.checkEmailUser(room.data().user2).then(user2 => {
@@ -46,41 +38,27 @@ export class AboutPage {
               dataTemp.photo = url;
               let msgTemp =[];
               this.chatService.getChat(this.user.email, dataTemp.email).onSnapshot(chat => {
-
                 if (chat.docs.length > 0) {
-                  console.log(msgTemp);
-
                   chat.docChanges.forEach(data => {
                     msgTemp.push(data.doc.data())
-                    // console.log(data.doc.data());
-                    
                   })
                 }
-
               })
               roomtemp.push({ friend: dataTemp, messages: msgTemp });
               this.rooms = roomtemp;
-              // loading.dismiss()
-              console.log(this.rooms);
-
             })
           } else {
             dataTemp.photo = "https://png.pngtree.com/svg/20170827/people_106508.png";
             let msgTemp = [];
             this.chatService.getChat(this.user.email, dataTemp.email).onSnapshot(chat => {
-
               if (chat.docs.length > 0) {
-                // msgTemp = [];
                 chat.docChanges.forEach(data => {
                   msgTemp.push(data.doc.data())
                 })
               }
-
             })
             roomtemp.push({ friend: dataTemp, messages: msgTemp });
             this.rooms = roomtemp;
-            // loading.dismiss()
-
           }
         })
       })
@@ -88,7 +66,6 @@ export class AboutPage {
   }
 
   onClickOpenChat(room) {
-    console.log(room);
     this.navCtrl.push(ChatPage, { room: room, user: this.user })
   }
 
--- src/pages/addfriend/addfriend.ts
+++ src/pages/addfriend/addfriend.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController, AlertController } from 'ionic-angular';
 import { UserServiceProvider } from '../../providers/user-service/user-service';
 import { FirebaseStorageProvider } from '../../providers/firebase-storage/firebase-storage';
-import { User } from 'firebase';
+import { User } from '../../models/users';
 
 /**
  * Generated class for the AddfriendPage page.
@@ -18,8 +18,8 @@ import { User } from 'firebase';
 })
 export class AddfriendPage {
 
-  email_friend;
-
+  email_friend: string = '';
+  checkFriend = true;
   data_friend;
   frieng_img;
 
@@ -58,13 +58,28 @@ export class AddfriendPage {
 
         if (data.docs[0]) {
           this.data_friend = data.docs[0].data()
-
           if (this.data_friend.photo == '') {
             this.frieng_img = "https://png.pngtree.com/svg/20170827/people_106508.png";
+            if (this.user.friends.length > 0) {
+              let findEmail = this.user.friends.find(this.findEmail)
+              if(findEmail){
+                this.checkFriend = true;
+              }else{
+                this.checkFriend = false;
+              }
+            }
           } else {
             this.firebaseSto.getURLImg(this.data_friend.email, this.data_friend.photo).then(url => {
               console.log(url);
               this.frieng_img = url;
+              if (this.user.friends.length > 0) {
+                let findEmail = this.user.friends.find(this.findEmail)
+                if(findEmail){
+                  this.checkFriend = true;
+                }else{
+                  this.checkFriend = false;
+                }
+              }
             })
           }
         } else {
@@ -81,6 +96,11 @@ export class AddfriendPage {
     }
   }
 
+  findEmail = (email) => {
+    console.log(email);
+    return email.email == this.email_friend;
+  }
+
   onClickADD() {
     this.userService.addFriend(this.user.email, this.email_friend).then(() => {
       console.log(65);
@@ -90,7 +110,8 @@ export class AddfriendPage {
           {
             text: 'OK',
             handler: () => {
-              this.data_friend = null
+              this.data_friend = null;
+              this.checkFriend = true;
             }
           }
         ]
